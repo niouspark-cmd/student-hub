2df6c5b902a5e7a9e76176a82998cd51cc4a8fd5
kingenious0-8358 <kingenious0@gmail.com>
2025-12-21 11:24:56 +0000

    fix: explicitly pass accelerate url to prisma client constructor to bypass validation error

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -2,10 +2,16 @@
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
+
 const prismaClientSingleton = () => {
+    const accelerateUrl = process.env.PRISMA_ACCELERATE_URL;
+    if (!accelerateUrl) {
+        throw new Error('PRISMA_ACCELERATE_URL is not set');
+    }
     return new PrismaClient({
         log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-    }).$extends(withAccelerate());
+        datasources: { db: { url: accelerateUrl } },
+    } as any).$extends(withAccelerate());
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
